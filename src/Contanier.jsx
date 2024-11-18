f6ef3052693c60e94440788688181028c89d38bb
abu-tal-ha <mabutalha468@gmail.com>
2024-11-18 20:14:53 +0600

    done

---
+++ src/Contanier.jsx
@@ -0,0 +1,34 @@
+
+/* eslint-disable react/prop-types */
+
+import All from "./All";
+import Menue from "./Menue";
+
+
+
+
+
+function Contanier({ model, setModel, onAdd }) {
+
+
+
+
+  return (
+    <>
+      <div className="mt-9 dark:bg-black border-b-2">
+        <div className="">
+          <div className="container mx-auto">
+            <Menue model={model} setModel={setModel} />
+            <All onAdd={onAdd} />
+
+
+          </div>
+        </div>
+
+      </div>
+
+    </>
+  );
+}
+
+export default Contanier;
---
+++ src/SearchDataLi.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+
+// import ImageOne from "./assets/book_images/1.jpg"
+
+
+function SearchDataLi({ item }) {
+
+  const { name, author, price, image } = item || {};
+
+
+  return (
+    <>
+
+      <li className="flex gap-4 h-[92px]  hover:bg-[#00D991] p-3 pl-5 mt-[14px]">
+        <img className="w-[60px] h-[60px]" src={image} alt="" />
+        <div className="">
+          <strong>{name}</strong>
+          <p>{author}</p>
+          <p>{price}</p>
+        </div>
+
+      </li>
+
+    </>
+  );
+}
+
+export default SearchDataLi;
